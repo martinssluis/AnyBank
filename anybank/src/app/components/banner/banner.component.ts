3bfe9a807b115e18cd01d04cd0653bf9c035a485
Luis Martins <luishenrique.martinslima@gmail.com>
2026-07-26 18:31:20 -0300

    feat(banner.component.ts): importa DatePipe  cria atributo dataAtual para ser usada dinamicamente no template

--- anybank/src/app/components/banner/banner.component.ts
+++ anybank/src/app/components/banner/banner.component.ts
@@ -1,11 +1,14 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css'
 })
 export class BannerComponent {
+
+  dataAtual = new Date();
 
 }
