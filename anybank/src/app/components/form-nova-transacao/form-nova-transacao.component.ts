db35e176671c6fa749c902277fc3c8a6d756fb32
Luis Martins <luishenrique.martinslima@gmail.com>
2026-07-27 10:56:24 -0300

    feat(form-nova-transacao.component.ts): cria atributo valorTransacao e metodo aoSubmeter para serem usados no template

--- anybank/src/app/components/form-nova-transacao/form-nova-transacao.component.ts
+++ anybank/src/app/components/form-nova-transacao/form-nova-transacao.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-nova-transacao',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form-nova-transacao.component.html',
   styleUrl: './form-nova-transacao.component.css'
 })
 export class FormNovaTransacaoComponent {
 
+  valorTransacao= "";
+
+  aoSubmeter() {
+    console.log(this.valorTransacao);
+}
 }
