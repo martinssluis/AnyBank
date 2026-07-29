dcba8dc63fbac797afe463d5c12828d4bf0bd74b
Luis Martins <luishenrique.martinslima@gmail.com>
2026-07-29 16:18:22 -0300

    feat(form-nova-transacao.componen.ts): cria atributo tipoTransacaoEnum que recebe TipoTransacao e importa o KeyValuePipe

--- anybank/src/app/components/form-nova-transacao/form-nova-transacao.component.ts
+++ anybank/src/app/components/form-nova-transacao/form-nova-transacao.component.ts
@@ -1,10 +1,11 @@
 import { Component, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Transacao, TipoTransacao } from '../../models/transacao';
+import { KeyValuePipe } from '@angular/common';
 
 @Component({
   selector: 'app-form-nova-transacao',
-  imports: [FormsModule],
+  imports: [FormsModule, KeyValuePipe],
   templateUrl: './form-nova-transacao.component.html',
   styleUrl: './form-nova-transacao.component.css'
 })
@@ -13,6 +14,7 @@ export class FormNovaTransacaoComponent {
   tipoTransacao = "";
   valorTransacao= "";
   trasacaoCriada = output<Transacao>();
+  tipoTransacaoEnum = TipoTransacao;
 
   aoSubmeter() {
 
