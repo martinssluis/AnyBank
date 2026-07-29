44857842cf0bd77b5065bdd3c4e568506b339588
Luis Martins <luishenrique.martinslima@gmail.com>
2026-07-29 12:13:38 -0300

    feat(app.component.ts): processarTransacao agora tem como parametro uma transacao do tipo Transacao e imprime a transacao passada pelo formulario

--- anybank/src/app/app.component.ts
+++ anybank/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from './components/banner/banner.component'
 import { FormNovaTransacaoComponent } from "./components/form-nova-transacao/form-nova-transacao.component";
+import { Transacao } from './models/transacao';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,7 @@ import { FormNovaTransacaoComponent } from "./components/form-nova-transacao/for
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  processarTransacao(){
-    console.log("transacao criada!")
+  processarTransacao(transacao: Transacao){
+    console.log(transacao);
   }
 }
