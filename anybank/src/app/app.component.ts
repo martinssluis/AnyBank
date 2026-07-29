3dd19ff467d1151ecdb1f4240e24e9bf61d9543c
Luis Martins <luishenrique.martinslima@gmail.com>
2026-07-29 15:19:30 -0300

    feat(app.component.ts): cria atributo saldo que recebe um computed para que sempre que processarmos uma transacao o computed será acionado

--- anybank/src/app/app.component.ts
+++ anybank/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { BannerComponent } from './components/banner/banner.component'
 import { FormNovaTransacaoComponent } from "./components/form-nova-transacao/form-nova-transacao.component";
 import { Transacao } from './models/transacao';
@@ -12,8 +12,13 @@ import { Transacao } from './models/transacao';
 export class AppComponent {
   transacoes = signal<Transacao[]>([]);
 
+  saldo = computed(()=>{
+    return this.transacoes().reduce((acc, transacaoAtual)=>{
+      return acc + transacaoAtual.valor;
+    }, 0)
+  });
+
   processarTransacao(transacao: Transacao){
     this.transacoes.update((listaAtual)=>[transacao, ...listaAtual]) // faz as transações mais recentes serem mostradas primeiro
-    console.log(this.transacoes())
   }
 }
