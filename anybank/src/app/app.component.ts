7a37a5fd451dec6642a7f2735d620a42348a71d1
Luis Martins <luishenrique.martinslima@gmail.com>
2026-07-29 15:24:16 -0300

    feat(app.component.ts): cria estruturas condicionais baseadas do TipoTransacao para efetuar um DEPOSITO ou um SAQUE

--- anybank/src/app/app.component.ts
+++ anybank/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, computed, signal } from '@angular/core';
 import { BannerComponent } from './components/banner/banner.component'
 import { FormNovaTransacaoComponent } from "./components/form-nova-transacao/form-nova-transacao.component";
-import { Transacao } from './models/transacao';
+import { TipoTransacao, Transacao } from './models/transacao';
 
 @Component({
   selector: 'app-root',
@@ -14,7 +14,14 @@ export class AppComponent {
 
   saldo = computed(()=>{
     return this.transacoes().reduce((acc, transacaoAtual)=>{
-      return acc + transacaoAtual.valor;
+      if(transacaoAtual.tipo == TipoTransacao.DEPOSITO){
+        return acc + transacaoAtual.valor;
+      }
+      else if (transacaoAtual.tipo == TipoTransacao.SAQUE){
+        return acc - transacaoAtual.valor;
+      } else{
+        throw new Error('Tipo de transação não identificado')
+      }
     }, 0)
   });
 
